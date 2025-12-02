48514bf67d148bafefb049500c038672c91b167d
vboxuser <vboxuser@Ubuntu-22-04.myguest.virtualbox.org>
2025-12-02 02:51:38 -0500

    Lab6 Assignment 3: Progress for the developement of the full ekf localization filter

--- prob_rob_labs/src/ekf_localization/ekf_localization.py
+++ prob_rob_labs/src/ekf_localization/ekf_localization.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 import yaml
 import os
 import numpy as np
+import math
 
 from sensor_msgs.msg import CameraInfo
 from prob_rob_msgs.msg import Point2DArrayStamped, Point2D
@@ -11,6 +12,16 @@ Min_pts = 4
 Min_x_margin_percentage = 0.02
 Min_y_margin_percentage = 0.03
 y_max_percentage = 0.625
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import TransformStamped
+from tf_transformations import quaternion_from_euler
+from tf2_ros import TransformBroadcaster
+
+EPS_ANG = 1e-6
+CAM_OFFSET = np.array([0.076, 0, 0.093])
+
+def wrap_angle(a):
+    return (a + np.pi) % (2.0 * np.pi) - np.pi
 
 class EkfLocalization(Node):
 
@@ -19,6 +30,13 @@ class EkfLocalization(Node):
         self.log = self.get_logger()
 
         self.declare_parameter('map_file', '')
+        self.declare_parameter('alpha_v', 0.02)
+        self.declare_parameter('alpha_w', 0.02) 
+        self.declare_parameter('meas_sigma_d_a', 0.017340)
+        self.declare_parameter('meas_sigma_d_b', 0.001990)
+        self.declare_parameter('meas_sigma_t_a', 0.000104)
+        self.declare_parameter('meas_sigma_t_b', 0.000088)
+
         map_file = self.get_parameter('map_file').value
 
         if map_file == '':
@@ -49,9 +67,7 @@ class EkfLocalization(Node):
         )
 
         self.measurement_pubs = {}
-
         for color, info in self.landmarks["landmarks"].items():
-
             meas_topic = f"/measurement_{color}"
             self.measurement_pubs[color] = self.create_publisher(
                 Point2DArrayStamped, meas_topic, 10
@@ -66,6 +82,27 @@ class EkfLocalization(Node):
             )
 
             self.log.info(f"Subscribed to {vision_topic} and publishing to {meas_topic}")
+
+        self.initial_x = -1.5
+        self.initial_y = 0.0
+        self.initial_theta = 0.0
+
+        self.x = np.array([[self.initial_theta], [self.initial_x], [self.initial_y]], dtype=float)
+        self.P = np.eye(3) * 1e-3
+        self.system_time = None
+        self.last_vel = 0.0
+        self.last_omega = 0.0
+
+        self.create_subscription(Odometry, '/ekf_odom', self.odom_cb, 10)
+        self.ekf_pub = self.create_publisher(Odometry, '/ekf_pose', 10)
+        self.tf_broadcaster = TransformBroadcaster(self)
+
+        self.alpha_v = self.get_parameter('alpha_v').value
+        self.alpha_w = self.get_parameter('alpha_w').value
+        self.meas_sigma_d_a = self.get_parameter('meas_sigma_d_a').value
+        self.meas_sigma_d_b = self.get_parameter('meas_sigma_d_b').value
+        self.meas_sigma_t_a = self.get_parameter('meas_sigma_t_a').value
+        self.meas_sigma_t_b = self.get_parameter('meas_sigma_t_b').value
 
         self.log.info("EKF Localization Node Initialized\n")
 
@@ -88,23 +125,43 @@ class EkfLocalization(Node):
 
         height = landmark_info["height"]
 
-        dist, theta = self.estimate_distance_bearing(corners_msg, height)
+        dist, bearing = self.estimate_distance_bearing(corners_msg, height)
 
-        if dist is None or theta is None:
+        if dist is None or bearing is None:
             return
+
+        t_meas = self.get_clock().now().nanoseconds * 1e-9
+        if self.system_time is None:
+            self.system_time = t_meas
+            return
+
+        dt = t_meas - self.system_time
+        self.log.warn(f"DEBUG: t_meas={t_meas:.6f}, system_time={self.system_time:.6f}, dt={dt:.6f}")
+        if dt < -0.02:
+            self.log.warn(f"Late measurement (dt={dt:.3f}s). Ignored.")
+            return
+
+        dt = max(dt, 0.0)
+
+        if dt > 0:
+            self.ekf_predict(dt)
+
+        self.system_time = t_meas
+
+        self.ekf_update(dist, bearing, landmark_info)
 
         msg = Point2DArrayStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "camera_link"
+        msg.header.frame_id = "camera_rgb_frame"
 
         pt = Point2D()
         pt.x = float(dist)
-        pt.y = float(theta)
+        pt.y = float(bearing)
         msg.points = [pt]
 
         self.measurement_pubs[color].publish(msg)
         self.log.info(
-            f"[{color}] Published: distance={dist:.3f} | bearing={theta:.3f}"
+            f"[{color}] Published: distance={dist:.3f} | bearing={bearing:.3f}"
         )
 
     def estimate_distance_bearing(self, corners_msg, landmark_height):
@@ -135,10 +192,178 @@ class EkfLocalization(Node):
         fx = self.p_matrix[0][0]
         fy = self.p_matrix[1][1]
 
-        theta = np.arctan((cx - x_center_perceived) / fx)
-        distance = landmark_height * fy / (height_perceived * np.cos(theta))
+        bearing = np.arctan((cx - x_center_perceived) / fx)
+        distance = landmark_height * fy / (height_perceived * np.cos(bearing))
+        return distance, bearing
 
-        return distance, theta
+    def odom_cb(self, odom_msg):
+        t_odom = odom_msg.header.stamp.sec + odom_msg.header.stamp.nanosec*1e-9
+
+        if self.system_time is None:
+            self.last_vel = odom_msg.twist.twist.linear.x
+            self.last_omega = odom_msg.twist.twist.angular.z
+            return
+
+        dt = t_odom - self.system_time
+        if dt < 0:
+            self.log.warn("Late odometry sample ignored")
+            return
+        if dt == 0:
+            self.last_vel = odom_msg.twist.twist.linear.x
+            self.last_omega = odom_msg.twist.twist.angular.z
+            return
+
+        self.last_vel = odom_msg.twist.twist.linear.x
+        self.last_omega = odom_msg.twist.twist.angular.z
+
+        self.ekf_predict(dt)
+        self.system_time = t_odom
+
+    def ekf_predict(self, dt):
+        theta = float(self.x[0, 0])
+        v = float(self.last_vel)
+        omega = float(self.last_omega)
+
+        if abs(omega) < EPS_ANG:
+            dtheta = 0.0
+            dx = v * dt * math.cos(theta)
+            dy = v * dt * math.sin(theta)
+        else:
+            dtheta = omega * dt
+            dx = (v / omega) * (math.sin(theta + omega * dt) - math.sin(theta))
+            dy = (v / omega) * (-math.cos(theta + omega * dt) + math.cos(theta))
+
+        theta_new = wrap_angle(theta + dtheta)
+        self.x[0, 0] = theta_new
+        self.x[1, 0] += dx
+        self.x[2, 0] += dy
+
+        G = np.eye(3)
+        if abs(omega) < EPS_ANG:
+            G[1, 0] = -v * dt * math.sin(theta)
+            G[2, 0] = v * dt * math.cos(theta)
+        else:
+            G[1, 0] = -(v / omega) * math.cos(theta) + (v / omega) * math.cos(theta + omega * dt)
+            G[2, 0] = -(v / omega) * math.sin(theta) + (v / omega) * math.sin(theta + omega * dt)
+
+        M = np.diag([self.alpha_v * (v ** 2 + 1e-6), self.alpha_w * (omega ** 2 + 1e-6)])
+
+        V = np.zeros((3, 2), dtype=float)
+        if abs(omega) < EPS_ANG:
+            V[1, 0] = dt * math.cos(theta)
+            V[2, 0] = dt * math.sin(theta)
+
+            V[0, 1] = dt
+            V[1, 1] = v * ( -0.5 * (dt ** 2) * math.sin(theta) )
+            V[2, 1] = v * ( 0.5 * (dt ** 2) * math.cos(theta) )
+        else:
+            V[1, 0] = (1.0 / omega) * (math.sin(theta + omega * dt) - math.sin(theta))
+            V[2, 0] = (1.0 / omega) * (-math.cos(theta + omega * dt) + math.cos(theta))
+
+            V[0, 1] = dt
+            V[1, 1] = v * ( (dt * math.cos(theta + omega * dt) * omega - math.sin(theta + omega * dt) + math.sin(theta)) / (omega ** 2) )
+            V[2, 1] = v * ( (dt * math.sin(theta + omega * dt) * omega + math.cos(theta + omega * dt) - math.cos(theta)) / (omega ** 2) )
+            
+        self.P = G @ self.P @ G.T + V @ M @ V.T
+
+    def ekf_update(self, dist, bearing, landmark_info):
+        theta = float(self.x[0,0])
+        x_r   = float(self.x[1,0])
+        y_r   = float(self.x[2,0])
+
+        dx = CAM_OFFSET[0]
+        dy = CAM_OFFSET[1]
+
+        x_cam = x_r + math.cos(theta)*dx - math.sin(theta)*dy
+        y_cam = y_r + math.sin(theta)*dx + math.cos(theta)*dy
+
+        lm_x = landmark_info["x"]
+        lm_y = landmark_info["y"]
+
+        dx_l = lm_x - x_cam
+        dy_l = lm_y - y_cam
+
+        pred_dist = math.hypot(dx_l, dy_l)
+        pred_bearing = math.atan2(dy_l, dx_l) - theta
+        pred_bearing = wrap_angle(pred_bearing)
+
+        q = dx_l**2 + dy_l**2
+        sqrt_q = math.sqrt(q)
+
+        H = np.zeros((2,3))
+
+        H[0,0] = 0
+        H[0,1] = -dx_l / sqrt_q
+        H[0,2] = -dy_l / sqrt_q
+
+        H[1,0] = -1
+        H[1,1] =  dy_l / q
+        H[1,2] = -dx_l / q
+
+        sigma_d2 = self.meas_sigma_d_a + self.meas_sigma_d_b * dist
+        sigma_t2 = self.meas_sigma_t_a + self.meas_sigma_t_b * dist
+        R = np.diag([sigma_d2, sigma_t2])
+
+        z = np.array([[dist],[bearing]])
+        z_pred = np.array([[pred_dist],[pred_bearing]])
+
+        y = z - z_pred
+        y[1,0] = wrap_angle(y[1,0])
+
+        S = H @ self.P @ H.T + R
+        K = self.P @ H.T @ np.linalg.inv(S)
+
+        self.x = self.x + K @ y
+        self.x[0,0] = wrap_angle(self.x[0,0])
+
+        self.P = (np.eye(3) - K @ H) @ self.P
+
+        self.publish_ekf_pose()
+
+    def publish_ekf_pose(self):
+        odom = Odometry()
+        odom.header.stamp = self.get_clock().now().to_msg()
+        odom.header.frame_id = "odom"
+        odom.child_frame_id = "base_footprint"
+
+        theta, x, y = self.x.flatten()
+        quat = quaternion_from_euler(0,0,theta)
+
+        odom.pose.pose.position.x = x
+        odom.pose.pose.position.y = y
+        odom.pose.pose.position.z = 0.0
+        odom.pose.pose.orientation.x = quat[0]
+        odom.pose.pose.orientation.y = quat[1]
+        odom.pose.pose.orientation.z = quat[2]
+        odom.pose.pose.orientation.w = quat[3]
+
+        pose_cov = np.zeros((6, 6))
+        pose_cov[0, 0] = self.P[1, 1]
+        pose_cov[0, 1] = self.P[1, 2]
+        pose_cov[0, 5] = self.P[1, 0]
+        pose_cov[1, 0] = self.P[2, 1]
+        pose_cov[1, 1] = self.P[2, 2]
+        pose_cov[1, 5] = self.P[2, 0]
+        pose_cov[5, 0] = self.P[0, 1]
+        pose_cov[5, 1] = self.P[0, 2]
+        pose_cov[5, 5] = self.P[0, 0]
+        odom.pose.covariance = pose_cov.flatten().tolist()
+
+        self.ekf_pub.publish(odom)
+
+        # t = TransformStamped()
+        # t.header.stamp = odom.header.stamp
+        # t.header.frame_id = "odom"
+        # t.child_frame_id = "base_footprint"
+        # t.transform.translation.x = x
+        # t.transform.translation.y = y
+        # t.transform.translation.z = 0.0
+        # t.transform.rotation.x = quat[0]
+        # t.transform.rotation.y = quat[1]
+        # t.transform.rotation.z = quat[2]
+        # t.transform.rotation.w = quat[3]
+
+        # self.tf_broadcaster.sendTransform(t)
 
     def spin(self):
         rclpy.spin(self)
