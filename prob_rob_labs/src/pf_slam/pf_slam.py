7614b1835536d6e06fd5fd41912d9a89ce4a7c9f
Prahlad Vivek <prahladvivek2001@gmail.com>
2025-12-09 20:54:22 -0500

    built pf prediction and update steps

--- prob_rob_labs/src/pf_slam/pf_slam.py
+++ prob_rob_labs/src/pf_slam/pf_slam.py
@@ -3,15 +3,22 @@ from rclpy.node import Node
 
 from sensor_msgs.msg import CameraInfo
 from prob_rob_msgs.msg import Point2DArrayStamped, Point2D
+from nav_msgs.msg import Odometry
 
 import os
 import yaml
 import numpy as np
+import math
 
 min_points = 4
 min_x_margin_percent = 0.02
 min_y_margin_percent = 0.03
 y_max_percent = 0.625
+
+CAM_OFFSET = np.array([0.076, 0, 0.093])
+
+def wrap_angle(a):
+    return (a + math.pi) % (2 * math.pi) - math.pi
 
 class PfSlam(Node):
 
@@ -39,6 +46,7 @@ class PfSlam(Node):
 
         #### Subscriptions and Publishers #####
         self.camera_sub = self.create_subscription(CameraInfo, '/camera/camera_info', self.camera_info_callback, 10)
+        
         self.measurement_pubs = {}
         for color, info in self.landmarks["landmarks"].items():
             meas_topic = f"/measurement_{color}"
@@ -54,11 +62,48 @@ class PfSlam(Node):
 
             self.log.info(f"Subscribed to {vision_topic} and publishing to {meas_topic}")
 
+        self.create_subscription(Odometry, '/ekf_odom', self.odom_callback, 10)
+
         #### Parameter declaring #####
         self.p_matrix = None
         self.img_h = None
         self.img_w = None 
         self.system_time = None
+        self.last_odom_time = None
+
+        self.last_vel = 0.0
+        self.last_omega = 0.0
+
+        self.alpha_v = self.get_parameter('alpha_v').value
+        self.alpha_w = self.get_parameter('alpha_w').value
+        self.meas_sigma_d_a = self.get_parameter('meas_sigma_d_a').value
+        self.meas_sigma_d_b = self.get_parameter('meas_sigma_d_b').value
+        self.meas_sigma_t_a = self.get_parameter('meas_sigma_t_a').value
+        self.meas_sigma_t_b = self.get_parameter('meas_sigma_t_b').value
+
+        self.num_particles = 100
+        self.particles = []
+        self.initialize_particles()
+
+    def initialize_particles(self):
+        self.particles = []
+        initial_theta = 0.0
+        initial_x = -1.5
+        initial_y = 0.0
+
+        for _ in range (self.num_particles):
+            theta = initial_theta + np.random.normal(0, 0.05)
+            x = initial_x + np.random.normal(0, 0.1)
+            y = initial_y + np.random.normal(0, 0.1)
+
+            particle = {
+                "theta": theta,
+                "x": x, 
+                "y": y,
+                "weight": 1.0 / self.num_particles,
+                "landmarks": {}
+            }
+            self.particles.append(particle)
 
     def estimate_distance_bearing(self, corners_msg, landmark_height):
         points = corners_msg.points
@@ -133,8 +178,11 @@ class PfSlam(Node):
 
         self.system_time = t_meas
 
-        ### insert pf measurement update step (call a function to do that?)
+        ### pf measurement update function
+        self.pf_update(color, dist, bearing)
 
+
+        ### sending as topic for debugging purpose
         msg = Point2DArrayStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "camera_rgb_frame"
@@ -149,7 +197,187 @@ class PfSlam(Node):
             f"[{color}] Published: distance={dist:.3f} | bearing={bearing:.3f}"
         )
     
-    
+    def odom_callback(self, odom_msg: Odometry):
+        t_odom = odom_msg.header.stamp.sec + odom_msg.header.stamp.nanosec*1e-9
+        vel = odom_msg.twist.twist.linear.x
+        omega = odom_msg.twist.twist.angular.z
+
+        if self.last_odom_time is None:
+            self.last_odom_time = t_odom
+            self.last_vel = vel
+            self.last_omega = omega
+            return
+
+        dt = t_odom - self.last_odom_time
+        self.last_odom_time = t_odom
+
+        if dt < 0:
+            self.log.warn("Late odometry sample ignored")
+            return
+        if dt == 0:
+            self.last_vel = odom_msg.twist.twist.linear.x
+            self.last_omega = odom_msg.twist.twist.angular.z
+            return
+
+        self.last_vel = vel
+        self.last_omega = omega
+
+        self.pf_predict(dt)
+        self.log.info("Valid odom update: dt={dt:.3f}, v={self.last_vel:.3f}, omega={self.last_omega:.3f}")
+
+    def pf_predict(self, dt: float):
+        ### to propagate all particles forward
+        
+        if self.last_vel is None or self.last_omega is None:
+            return ## haven't received odometry yet
+        
+        v = self.last_vel
+        omega = self.last_omega
+
+        sigma_v = math.sqrt(self.alpha_v * (v ** 2 + 1e-6))
+        sigma_w = math.sqrt(self.alpha_w * (omega ** 2 + 1e-6))
+
+        EPS_ANG = 1e-6
+
+        for p in self.particles:
+            theta = p["theta"]
+            x = p["x"]
+            y = p["y"]
+
+            v_sample = v + np.random.normal(0.0, sigma_v)
+            w_sample = omega + np.random.normal(0.0, sigma_w)
+
+            if abs(w_sample) < EPS_ANG:
+                dtheta = 0.0
+                dx = v_sample * dt * math.cos(theta)
+                dy = v_sample * dt * math.cos(theta)
+            else:
+                dtheta = w_sample * dt
+                dx = (v_sample / w_sample) * (math.sin(theta + w_sample * dt) - math.sin(theta))
+                dy = (v_sample / w_sample) * (-math.cos(theta + w_sample * dt) + math.cos(theta))
+
+            theta_new = wrap_angle(theta + dtheta)
+
+            p["theta"] = theta_new
+            p["x"] = x + dx
+            p["y"] = y + dy
+
+    def pf_update(self, color: str, dist: float, bearing: float):
+        '''
+            measurement update for 1 landmark at a time;
+            per particle landmark update
+
+        '''
+        if self.p_matrix is None:
+            return
+        
+        if not self.particles:
+            return
+        
+        sigma_d2 = self.meas_sigma_d_a + self.meas_sigma_d_b * dist
+        sigma_t2 = self.meas_sigma_t_a + self.meas_sigma_t_b * dist
+        R = np.diag([sigma_d2, sigma_t2])
+
+        z = np.array([[dist], [bearing]])
+
+        for p in self.particles:
+            theta = p["theta"]
+            x_r = p["x"]
+            y_r = p["y"]
+
+            dx_cam = CAM_OFFSET[0]
+            dy_cam = CAM_OFFSET[1]
+
+            x_cam = x_r + math.cos(theta) * dx_cam - math.sin(theta) * dy_cam
+            y_cam = y_r + math.sin(theta) * dx_cam + math.cos(theta) * dy_cam
+
+            if color not in p["landmarks"]:
+                global_bearing = theta + bearing
+                lm_x = x_cam + dist * math.cos(global_bearing)
+                lm_y = y_cam + dist * math.sin(global_bearing)
+
+                mu = np.array([[lm_x], [lm_y]])
+
+                # taking jacobian J = dm/dz
+                J = np.zeros((2, 2))
+                J[0, 0] = math.cos(global_bearing)
+                J[0, 1] = -dist * math.sin(global_bearing)
+                J[1, 0] = math.sin(global_bearing)
+                J[1, 1] =  dist * math.cos(global_bearing)
+
+                sigma = J @ R @ J.T
+
+                p["landmarks"][color] = {
+                    "mu": mu,
+                    "Sigma": sigma
+                    }
+                continue
+
+            lm = p["landmarks"][color]
+            mu = lm["mu"]        
+            sigma = lm["Sigma"]  
+
+            lm_x = float(mu[0, 0])
+            lm_y = float(mu[1, 0])
+
+            dx_l = lm_x - x_cam
+            dy_l = lm_y - y_cam
+
+            q = dx_l**2 + dy_l**2
+            if q < 1e-9:
+                continue
+            pred_dist = math.sqrt(q)
+            pred_bearing = math.atan2(dy_l, dx_l) - theta
+            pred_bearing = wrap_angle(pred_bearing)
+
+            z_pred = np.array([[pred_dist],
+                            [pred_bearing]])
+            
+            # Innovation Step
+            y = z - z_pred
+            y[1, 0] = wrap_angle(y[1, 0])
+
+            # Jacobian wrt landmark position
+            r = pred_dist
+            H = np.zeros((2, 2))
+            H[0, 0] = dx_l / r
+            H[0, 1] = dy_l / r
+            H[1, 0] = -dy_l / q
+            H[1, 1] =  dx_l / q
+
+            S = H @ sigma @ H.T + R
+
+            try:
+                S_inv = np.linalg.inv(S)
+            except np.linalg.LinAlgError:
+                continue
+
+            K = sigma @ H.T @ S_inv
+
+            # EKF landmark update
+            mu_new = mu + K @ y
+            sigma_new = (np.eye(2) - K @ H) @ sigma
+
+            lm["mu"] = mu_new
+            lm["Sigma"] = sigma_new
+
+            # measurement likelihood for this particle
+
+            exponent = -0.5 * float(y.T @ S_inv @ y)
+            likelihood = math.exp(exponent)
+
+            p["weight"] *= likelihood
+
+        # normalizing particle weights
+        total_w = sum(p["weight"] for p in self.particles)
+        if total_w <= 0.0:
+            uniform_w = 1.0/len(self.particles)
+            for p in self.particles:
+                p["weight"] = uniform_w
+        else:
+            for p in self.particles:
+                p["weight"] /= total_w
+
     def spin(self):
         rclpy.spin(self)
 
